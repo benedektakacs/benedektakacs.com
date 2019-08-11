8ae813557595c134684c51356bd53c77eeeaf0cb
benedektakacs <54002946+benedektakacs@users.noreply.github.com>
2019-08-11 20:15:01 +0200

    Update personal-info.tsx

--- src/components/personal-info.tsx
+++ src/components/personal-info.tsx
@@ -6,7 +6,7 @@ export const PersonalInfo = () => (
       <td>Benedek Takács</td>
       <td>Graphic Design<br />
         Type Design<br />
-        Art Direction</td>     
+        Art Direction<br />    
       </td>
     </tr>
       <td>Education</td>
@@ -14,12 +14,23 @@ export const PersonalInfo = () => (
           MOME	Budapest, Hungary [2014–17]<br />
           UdK		Berlin, Germany [2015–16]<br />
       </td>
-   <tr>
+    <tr>
       <td>Awards and Honors</td>
-      <td>Graphic Design<br />
-        Type Design<br />
-        Art Direction</td>     
+      <td>Swiss Goverment Excellence Scholarship<br />
+        Hungarian Design Award<br />
+        Miniature Book Society [USA] Student Grant<br />
+        Prix ECAL    
       </td>
+    <tr>
+      <td>Contact</td>
+      <td>mail[at]benedektakacs.com<br />
+        Zürich<br />    
+      </td>
+    <tr>
+      <td>All Rights Reserved</td>
+      <td>2019<br /> 
+      </td>
+  
   
   </table>
 );
