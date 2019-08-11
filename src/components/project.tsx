d3c31b83bba227d3c92d46045d5914ff9db731f8
Greegko <greegko+dev@gmail.com>
2019-08-11 21:21:30 +0200

    add images style

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -11,7 +11,45 @@ import "./project.scss";
 
 export const Project = ({ title, description, images }: ProjectInterface) => (
   <div className="project" id={normalizeText(title)}>
-    {images.map(x => <img src={x} />)}<br />
+    <Images images={images} />
     <span className="title">{title}</span> <span className="description">{description}</span>
   </div>
 );
+
+const Images = ({ images }: { images: string[] }) => {
+  const imagesSrc = images.map(x => require("../data/images/"+ x));
+
+  const nodes = [];
+
+  while(imagesSrc.length > 0) {
+    const a = imagesSrc.shift();
+    const b = imagesSrc.shift();
+
+    nodes.push(<ImageRow aImage={a} bImage={b}></ImageRow>);
+  }
+
+  return nodes as any;
+}
+
+const ImageRow = ({ aImage, bImage }: { aImage: string, bImage?: string}) => {
+  if(!bImage) {
+    return (
+      <div className="images images--one">
+        <div>
+          <img src={aImage} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="images images--two">
+        <div>
+          <img src={aImage} />
+        </div>
+        <div>
+          <img src={bImage} />
+        </div>
+      </div>
+    )
+  }
+}
