17fc842e38e54ffe62eb37d0e3623ea5848470a9
Greegko <greegko+dev@gmail.com>
2019-08-11 20:20:03 +0200

    html fix

--- src/components/personal-info.tsx
+++ src/components/personal-info.tsx
@@ -2,22 +2,20 @@ import * as React from 'react';
 
 export const PersonalInfo = () => (
   <table>
-   <tr>
+    <tr>
       <td>Benedek Takács</td>
       <td>Graphic Design<br />
         Type Design<br />
         Art Direction<br />    
       </td>
-      </tr>
-    
+    </tr>
     <tr>
       <td>Education</td>
       <td>ECAL	Lausanne, Switzerland [2017–19]<br />   	
           MOME	Budapest, Hungary [2014–17]<br />
           UdK		Berlin, Germany [2015–16]<br />
       </td>
-      </tr>
-
+    </tr>
     <tr>
       <td>Awards and Honors</td>
       <td>Swiss Goverment Excellence Scholarship<br />
@@ -25,17 +23,17 @@ export const PersonalInfo = () => (
         Miniature Book Society [USA] Student Grant<br />
         Prix ECAL    
       </td>
- 
-    
+    </tr>
+    <tr>
       <td>Contact</td>
       <td>mail[at]benedektakacs.com<br />
         Zürich<br />    
       </td>
+    </tr>
     <tr>
       <td>All Rights Reserved</td>
       <td>2019<br /> 
       </td>
-  
-  
+    </tr>
   </table>
 );
