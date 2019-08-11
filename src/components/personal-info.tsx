76b92978c55dae586efbe07df7b108e026466131
benedektakacs <54002946+benedektakacs@users.noreply.github.com>
2019-08-11 20:18:19 +0200

    Update personal-info.tsx

--- src/components/personal-info.tsx
+++ src/components/personal-info.tsx
@@ -2,18 +2,22 @@ import * as React from 'react';
 
 export const PersonalInfo = () => (
   <table>
-    <tr>
+   <tr>
       <td>Benedek Takács</td>
       <td>Graphic Design<br />
         Type Design<br />
         Art Direction<br />    
       </td>
-    </tr>
+      </tr>
+    
+    <tr>
       <td>Education</td>
       <td>ECAL	Lausanne, Switzerland [2017–19]<br />   	
           MOME	Budapest, Hungary [2014–17]<br />
           UdK		Berlin, Germany [2015–16]<br />
       </td>
+      </tr>
+
     <tr>
       <td>Awards and Honors</td>
       <td>Swiss Goverment Excellence Scholarship<br />
@@ -21,7 +25,8 @@ export const PersonalInfo = () => (
         Miniature Book Society [USA] Student Grant<br />
         Prix ECAL    
       </td>
-    <tr>
+ 
+    
       <td>Contact</td>
       <td>mail[at]benedektakacs.com<br />
         Zürich<br />    
