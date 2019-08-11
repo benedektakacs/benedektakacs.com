92b8cd6307b3e612e26ae19b812be7b5db7bbf38
Greegko <greegko+dev@gmail.com>
2019-08-11 20:54:51 +0200

    add app style

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import { Menu } from './menu';
 import { PersonalInfo } from './personal-info';
 import { ProjectList } from './project-list';
-
-import "./app.scss";
 import { Project } from '../interface';
 
 interface AppInterface {
   projects: Project[];
 }
 
+import "./app.scss";
 export const App = ({ projects }: AppInterface) => (
   <div className="page">
     <Menu projects={projects}></Menu>
