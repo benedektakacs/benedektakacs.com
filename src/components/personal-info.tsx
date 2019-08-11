fe6fc82c917c247801847e986dba73364d624a82
benedektakacs <54002946+benedektakacs@users.noreply.github.com>
2019-08-11 20:07:17 +0200

    Update personal-info.tsx

--- src/components/personal-info.tsx
+++ src/components/personal-info.tsx
@@ -3,11 +3,23 @@ import * as React from 'react';
 export const PersonalInfo = () => (
   <table>
     <tr>
-      <td>Benedek Takacs</td>
-      <td>
-        Graphic Design<br />
-        Type desing
+      <td>Benedek Takács</td>
+      <td>Graphic Design<br />
+        Type Design<br />
+        Art Direction</td>     
       </td>
     </tr>
+      <td>Education</td>
+      <td>ECAL	Lausanne, Switzerland [2017–19]<br />   	
+          MOME	Budapest, Hungary [2014–17]<br />
+          UdK		Berlin, Germany [2015–16]<br />
+      </td>
+   <tr>
+      <td>Awards and Honors</td>
+      <td>Graphic Design<br />
+        Type Design<br />
+        Art Direction</td>     
+      </td>
+  
   </table>
 );
