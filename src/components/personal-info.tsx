47655bd80d73205fa3fdfe0e1ce4347bc529a4e0
benedektakacs <54002946+benedektakacs@users.noreply.github.com>
2019-08-19 19:14:42 +0200

    Update personal-info.tsx

--- src/components/personal-info.tsx
+++ src/components/personal-info.tsx
@@ -13,17 +13,17 @@ export const PersonalInfo = () => (
     </tr>
     <tr>
       <td>Education</td>
-      <td>ECAL	Lausanne, Switzerland [2017–19]<br />   	
-          MOME	Budapest, Hungary [2014–17]<br />
-          UdK		Berlin, Germany [2015–16]<br />
+      <td>ECAL	Lausanne CH '19<br />   	
+          MOME	Budapest HU '17<br />
+          UdK		Berlin DE '16<br />
       </td>
     </tr>
     <tr>
       <td>Awards and Honors</td>
-      <td>Swiss Goverment Excellence Scholarship<br />
+      <td>Prix ECAL<br />
+        Swiss Government Excellence Scholarship<br />
         Hungarian Design Award<br />
-        Miniature Book Society [USA] Student Grant<br />
-        Prix ECAL<br />    
+        Miniature Book Society [USA] Student Grant<br />    
       </td>
     </tr>
     <tr>
