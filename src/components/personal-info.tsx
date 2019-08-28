c1cfc0b54b7fa1ba893ba5a944f9db9ae0c8f6b6
benedektakacs <54002946+benedektakacs@users.noreply.github.com>
2019-08-28 12:22:05 +0200

    Update personal-info.tsx

--- src/components/personal-info.tsx
+++ src/components/personal-info.tsx
@@ -10,7 +10,7 @@ export const PersonalInfo = () => (
         Type Design<br />
         Art Direction<br />
         <br />
-        Benedek Takács made his studies in graphic design and type design. He is recently completed his master degree at ECAL (CH) with qualification 'Excellent' and Prix EXECAL prize for the quality of his thesis. As his choosen topic of research and design of the master diploma shows a deep interest in the world of miniature books.<br />
+        Benedek Takács made his studies in graphic design and type design. He has recently completed his master degree at ECAL (CH) with qualification 'Excellent' and Prix EXECAL prize for the quality of his thesis. As his chosen topic of research and design of the master diploma shows a deep interest in the world of miniature books.<br />
         <br />
             
       </td>
